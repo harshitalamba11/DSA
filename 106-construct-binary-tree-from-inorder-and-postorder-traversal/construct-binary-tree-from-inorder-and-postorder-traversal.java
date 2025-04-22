06285fa68a3a220b67cebdefc705fec320307d45
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-04-23 01:14:21 +0530

    Time: 2 ms (56.86%) | Memory: 44.2 MB (79.60%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,6 +14,29 @@
  * }
  */
 class Solution {
+    public TreeNode helper(int[] inorder,int[] postorder,Map<Integer,Integer> map,int idx,int left,int right){
+        if(left>right) return null;
+        TreeNode root=new TreeNode(postorder[idx]);
+        int mid=map.get(root.val);
+        int right_len=right-mid;
+        root.right=helper(inorder,postorder,map,idx-1,mid+1,right);
+        root.left=helper(inorder,postorder,map,idx-right_len-1,left,mid-1);
+        return root;
+    }
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<postorder.length;i++){
+            map.put(inorder[i],i);   //basically map is storing the indexes so that we get the mid from it :)
+        }
+        return helper(inorder,postorder,map,postorder.length-1,0,postorder.length-1);
+    }
+}
+
+
+
+
+/*
+class Solution {
     public TreeNode helper(int[] inorder,int[] postorder,int in_start,int in_end,int post_start,int post_end){
         if(in_start>in_end) return null;
         TreeNode root=new TreeNode(postorder[post_end]);
@@ -35,3 +58,4 @@ class Solution {
         return helper(inorder,postorder,0,inorder_len-1,0,postorder_len-1);
     }
 }
+*/
