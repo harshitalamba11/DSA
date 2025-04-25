0b58fb42b41cd6299018bfb13118055e1b54191c
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-04-25 20:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (15.76%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -16,57 +16,28 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res=new ArrayList<>();
-        Stack<TreeNode> s=new Stack<>();
-        if(root==null){
-            return res;
-        }
-        s.push(root);
-        while(!s.isEmpty()){
-            TreeNode a=s.pop();
-            res.add(a.val);
-            if(a.right!=null){
-                s.push(a.right);
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left==null){
+                res.add(curr.val);
+                curr=curr.right;
             }
-            if(a.left!=null){
-                s.push(a.left);
+            else{
+                TreeNode leftChild=curr.left;
+                while(leftChild.right!=null && leftChild.right!=curr){
+                    leftChild=leftChild.right;
+                }
+                if(leftChild.right==null){
+                    res.add(curr.val);
+                    leftChild.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    leftChild.right=null;
+                    curr=curr.right;
+                }
             }
-            
         }
         return res;
     }
 }
-
-
-/*
-
-class Solution {
-    public void helper(TreeNode root,List<Integer> r){
-        
-        if (root==null) return ;
-        r.add(root.val);
-        helper(root.left,r);
-        helper(root.right,r);
-    }
-    public void iterativeapproach(TreeNode root,List<Integer> r){
-        Stack<TreeNode> s=new Stack<>();
-        if(root!=null){
-            s.push(root);
-        }
-        while(!s.isEmpty()){
-            TreeNode newnode=s.pop();
-            r.add(newnode.val);
-            if(newnode.right!=null){
-                s.push(newnode.right);
-            }
-            if(newnode.left!=null){
-                s.push(newnode.left);
-            }
-        }
-    }
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> r=new ArrayList<>();
-        iterativeapproach(root,r);
-        return r;
-    }
-}
-*/
