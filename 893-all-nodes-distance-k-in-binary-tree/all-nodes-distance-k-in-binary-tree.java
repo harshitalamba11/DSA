922ed4a483ccb91a13eac75cbff9b6fe5a07e212
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-04-30 01:44:06 +0530

    Time: 14 ms (39.17%) | Memory: 43 MB (26.15%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,41 +8,41 @@
  * }
  */
 class Solution {
-    public void findParent(HashMap<TreeNode,TreeNode> map,TreeNode root){
+    public void parent(TreeNode root,HashMap<TreeNode,TreeNode> map){
         if(root==null) return;
-        if(root.left!=null){
-            map.put(root.left,root);
-        }
-        if(root.right!=null){
-            map.put(root.right,root);
-        }
-        findParent(map,root.left);
-        findParent(map,root.right);
+        if(root.left!=null) map.put(root.left,root);
+        if(root.right!=null) map.put(root.right,root);
+        parent(root.left,map);
+        parent(root.right,map);
         return;
     }
-
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        List<Integer> res=new ArrayList<>();
-        HashMap<TreeNode,TreeNode> parentMap=new HashMap<>();
         Queue<TreeNode> q=new LinkedList<>();
+        List<Integer> res=new ArrayList<>();
         HashSet<TreeNode> visited=new HashSet<>();
+        HashMap<TreeNode,TreeNode> map=new HashMap<>();
         if(root==null) return res;
-        findParent(parentMap,root);
-        q.add(target);
+        parent(root,map);
+        q.add(target); // Start BFS from the target node
+        visited.add(target);
         while(!q.isEmpty()){
             int size=q.size();
             for(int i=0;i<size;i++){
                 TreeNode curr=q.poll();
                 visited.add(curr);
                 if(k==0) res.add(curr.val);
-                if(parentMap.containsKey(curr) && !visited.contains(parentMap.get(curr))){
-                    q.add(parentMap.get(curr));
+                
+                if(map.containsKey(curr) && !visited.contains(map.get(curr))){
+                    q.add(map.get(curr));
+                    visited.add(map.get(curr));
                 }
                 if(curr.left!=null && !visited.contains(curr.left)){
                     q.add(curr.left);
+                    visited.add(curr.left);
                 }
                 if(curr.right!=null && !visited.contains(curr.right)){
                     q.add(curr.right);
+                    visited.add(curr.right);
                 }
             }
             k--;
