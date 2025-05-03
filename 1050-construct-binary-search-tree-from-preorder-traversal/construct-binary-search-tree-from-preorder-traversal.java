4a8bc0be6f24fed57f2e5a517fb00aba52674417
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-05-03 16:37:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (42.16%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -14,32 +14,50 @@
  * }
  */
 class Solution {
-    public void insertNode(TreeNode root,int val){
-        if(root==null) return;
-        if(root.val>val){
-            if(root.left==null){
-                root.left=new TreeNode(val);;
-            }
-            else{
-                insertNode(root.left,val);
-            }
-        }
-        else{
-            if(root.right==null){
-                root.right=new TreeNode(val);;
-            }
-            else{
-                insertNode(root.right,val);
-            }
-        }
+
+    //iterative method 
+    // public void insertNode(TreeNode root,int val){
+    //     if(root==null) return;
+    //     if(root.val>val){
+    //         if(root.left==null){
+    //             root.left=new TreeNode(val);;
+    //         }
+    //         else{
+    //             insertNode(root.left,val);
+    //         }
+    //     }
+    //     else{
+    //         if(root.right==null){
+    //             root.right=new TreeNode(val);;
+    //         }
+    //         else{
+    //             insertNode(root.right,val);
+    //         }
+    //     }
+    // }
+    // public TreeNode bstFromPreorder(int[] preorder) {
+    //     if(preorder.length==0) return null;
+    //     TreeNode root=new TreeNode(preorder[0]);
+    //     TreeNode curr=root;
+    //     for(int i=1;i<preorder.length;i++){
+    //         insertNode(curr,preorder[i]);
+    //     }
+    //     return root;
+    // }
+
+
+
+    //recursive bound method
+    public int i=0;
+    public TreeNode helper(int[] preorder,int bound){
+        if(i==preorder.length || preorder[i]>bound) return null;
+        TreeNode root=new TreeNode(preorder[i++]);
+        root.left=helper(preorder,root.val);
+        root.right=helper(preorder,bound);
+        return root;
     }
     public TreeNode bstFromPreorder(int[] preorder) {
-        if(preorder.length==0) return null;
-        TreeNode root=new TreeNode(preorder[0]);
-        TreeNode curr=root;
-        for(int i=1;i<preorder.length;i++){
-            insertNode(curr,preorder[i]);
-        }
-        return root;
+        return helper(preorder,Integer.MAX_VALUE);
+
     }
 }
