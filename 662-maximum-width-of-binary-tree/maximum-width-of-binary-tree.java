cbf3988f66ad9ac6f19452cc9a2e1d256168b599
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-05-10 03:42:15 +0530

    Time: 2 ms (39.07%) | Memory: 43.4 MB (65.98%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -16,27 +16,56 @@
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         if(root==null) return 0;
-        int maxwidth=0;
         Queue<Pair<TreeNode,Integer>> q=new LinkedList<>();
         q.add(new Pair<>(root,0));
+        int maxwidth=0;
         while(!q.isEmpty()){
             int size=q.size();
-            int leftidx=q.peek().getValue();
-            int rightidx=leftidx;
+            TreeNode firstNode=q.peek().getKey();
+            TreeNode curr=firstNode;
+            int firstidx=q.peek().getValue();
+            int lastidx=firstidx;
             for(int i=0;i<size;i++){
-                Pair<TreeNode,Integer> current=q.poll();
-                TreeNode currentnode=current.getKey();
-                int currentidx=current.getValue();
-                rightidx=currentidx;
-                if(currentnode.left!=null){
-                    q.add(new Pair<>(currentnode.left,2*currentidx));
+                TreeNode curr1=q.peek().getKey();
+                lastidx=q.poll().getValue();
+                if(curr1.left!=null){
+                    q.add(new Pair<>(curr1.left,2*lastidx));
                 }
-                if(currentnode.right!=null){
-                    q.add(new Pair<>(currentnode.right, 2 * currentidx + 1));
+                if(curr1.right!=null){
+                    q.add(new Pair<>(curr1.right,2*lastidx+1));
                 }
             }
-            maxwidth=Math.max(maxwidth,rightidx-leftidx+1);
+            maxwidth=Math.max(maxwidth,lastidx-firstidx+1);
         }
         return maxwidth;
+
+
+
+        // if(root==null) return 0;
+        // int maxwidth=0;
+        // TreeNode curr=root;
+        // Queue<Pair<TreeNode,Integer>> q=new LinkedList<>();
+        // q.add(new Pair<>(curr,0));
+        // while(!q.isEmpty()){
+        //     TreeNode currNode=q.peek().getKey();
+        //     int leftidx=q.peek().getValue();
+        //     int rightidx=leftidx;
+        //     int size=q.size();
+        //     for(int i=0;i<size;i++){
+        //         Pair<TreeNode,Integer> p=q.poll();
+        //         TreeNode leftNode=p.getKey();
+        //         int curridx=p.getValue();
+        //         rightidx=curridx;
+        //         if(leftNode.left!=null){
+        //             q.add(new Pair<>(leftNode.left,2*curridx));
+        //         }
+        //         if(leftNode.right!=null){
+        //             q.add(new Pair<>(leftNode.right,2*curridx+1));
+        //         }
+        //     }
+            
+        // maxwidth=Math.max(maxwidth,rightidx-leftidx+1);
+        // }
+        // return maxwidth;
     }
 }
