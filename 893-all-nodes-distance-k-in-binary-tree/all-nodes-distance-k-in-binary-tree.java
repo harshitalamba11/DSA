91500730616f29ccd0c328d02871f9c89bee23e7
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-05-11 14:48:00 +0530

    Time: 12 ms (88.00%) | Memory: 42.6 MB (73.56%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -17,36 +17,32 @@ class Solution {
         return;
     }
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        Queue<TreeNode> q=new LinkedList<>();
         List<Integer> res=new ArrayList<>();
-        HashSet<TreeNode> visited=new HashSet<>();
+        HashSet<TreeNode> set=new HashSet<>();
+        Queue<TreeNode> q=new LinkedList<>();
         HashMap<TreeNode,TreeNode> map=new HashMap<>();
         if(root==null) return res;
         parent(root,map);
-        q.add(target); // Start BFS from the target node
-        visited.add(target);
+        q.add(target);
         while(!q.isEmpty()){
             int size=q.size();
             for(int i=0;i<size;i++){
                 TreeNode curr=q.poll();
-                visited.add(curr);
                 if(k==0) res.add(curr.val);
-                
-                if(map.containsKey(curr) && !visited.contains(map.get(curr))){
+                 if(root==null) return res;
+                set.add(curr);
+                if(map.containsKey(curr) && !set.contains(map.get(curr))){
                     q.add(map.get(curr));
-                    visited.add(map.get(curr));
                 }
-                if(curr.left!=null && !visited.contains(curr.left)){
+                if(curr.left!=null && !set.contains(curr.left)){
                     q.add(curr.left);
-                    visited.add(curr.left);
                 }
-                if(curr.right!=null && !visited.contains(curr.right)){
+                if(curr.right!=null && !set.contains(curr.right)){
                     q.add(curr.right);
-                    visited.add(curr.right);
                 }
+                
             }
-            k--;
-            if(k<0) break;
+            k--;if(k<0) break;
         }
         return res;
     }
