ef4ca70ed684356bff7b814629cb991709667add
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-04 16:46:09 +0530

    Time: 5 ms (32.08%) | Memory: 41.8 MB (68.31%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,29 +1,22 @@
 class Solution {
-    public List<String> helper(String s, List<String> wordDict, int idx) {
-        // if (memo.containsKey(idx)) return memo.get(idx);
-        
-        List<String> res = new ArrayList<>();
-        
-        if (idx == s.length()) {
-            res.add(""); // Base case: empty string as a valid suffix
+    public List<String> helper(String s,List<String> wordDict,int idx){
+        List<String> res=new ArrayList<>();
+        if(idx==s.length()){
+            res.add("");
             return res;
         }
-
-        for (int i = idx + 1; i <= s.length(); i++) {
-            String prefix = s.substring(idx, i);
-            if (wordDict.contains(prefix)) {
-                List<String> suffixes = helper(s, wordDict, i);
-                for (String suffix : suffixes) {
-                    res.add(prefix + (suffix.equals("") ? "" : " " + suffix));
+        for(int i=idx+1;i<=s.length();i++){
+            String prefix=s.substring(idx,i);
+            if(wordDict.contains(prefix)){
+                List<String> suffixs=helper(s,wordDict,i);
+                for(String suffix:suffixs){
+                    res.add(prefix+(suffix.equals("")?"":" ")+suffix);
                 }
             }
         }
-
-        // memo.put(idx, res);
         return res;
     }
-
     public List<String> wordBreak(String s, List<String> wordDict) {
-        return helper(s, wordDict, 0);
+        return helper(s,wordDict,0);
     }
 }
