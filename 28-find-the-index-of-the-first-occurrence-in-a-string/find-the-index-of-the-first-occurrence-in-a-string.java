ade5a94fb42171f6b3b2feefbff6670968dc6c4b
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-12 00:16:55 +0530

    Time: 1 ms (21.51%) | Memory: 41.5 MB (64.49%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,73 +1,47 @@
 class Solution {
     public int strStr(String s1, String s2) {
         if (s2.isEmpty()) return 0;
-
-        // Step 1: Build LPS array
-        int[] lps = new int[s2.length()];
-        int len = 0;
-        int i = 1;
-
-        while (i < s2.length()) {
-            if (s2.charAt(i) == s2.charAt(len)) {
+        int[] lps=new int[s2.length()];
+        lps[0]=0;
+        int len=0;
+        int i=1;
+        while(i<s2.length()){
+            if(s2.charAt(i)==s2.charAt(len)){
                 len++;
-                lps[i] = len;
+                lps[i]=len;
                 i++;
-            } else {
-                if (len != 0) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i] = 0;
+            }
+            else{
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
                     i++;
                 }
             }
         }
 
-        // Step 2: Pattern Search
-        int k = 0; // for s2
-        int l = 0; // for s1
-
-        while (l < s1.length()) {
-            if (s1.charAt(l) == s2.charAt(k)) {
-                l++;
-                k++;
-                if (k == s2.length()) return l - k;
-            } else {
-                if (k != 0) {
-                    k = lps[k - 1];
-                } else {
-                    l++;
+        i=0;
+        int j=0;
+        while(i<s1.length()){
+            if(s1.charAt(i)==s2.charAt(j)){
+                i++;
+                j++;
+                if(j==s2.length()) return i-j;
+            }
+            else{
+                if(j!=0){
+                    j=lps[j-1];
+                }
+                else{
+                    i++;
                 }
             }
         }
-
         return -1;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -106,41 +80,47 @@ class Solution {
 
 
         //method-4
-        // int[] lps=new int[s2.length()];
-        // int len=0;
-        // lps[0]=0;
-        // for(int i=1;i<s2.length();i++){
-        //     if(s2.charAt(i)==s2.charAt(len)){
-        //         len++;
-        //         lps[i]=len;
-        //     }
-        //     else{
-        //         if (len != 0) {
-        //             len = lps[len - 1];
-        //         } else {
-        //             lps[i] = 0;
-        //             // i++;
-        //         }
-        //     }
-        // }
+        // public int strStr(String s1, String s2) {
+//         if (s2.isEmpty()) return 0;
 
-        // int k=0;
-        // int l=0;
-        // while(l<s1.length()){
-        //     if(s1.charAt(l)==s2.charAt(k)){
-        //         l++;
-        //         k++;
-        //         if (k == s2.length()) return l - k;
-        //     }
-        //     else{
-        //         if (k != 0) {
-        //             k= lps[k - 1]; // use lps to skip
-        //         }
-        //         else {
-        //             l++;
-        //         }
-        //     }
-        // }
-        // return -1;
+//         // Step 1: Build LPS array
+//         int[] lps = new int[s2.length()];
+//         int len = 0;
+//         int i = 1;
+
+//         while (i < s2.length()) {
+//             if (s2.charAt(i) == s2.charAt(len)) {
+//                 len++;
+//                 lps[i] = len;
+//                 i++;
+//             } else {
+//                 if (len != 0) {
+//                     len = lps[len - 1];
+//                 } else {
+//                     lps[i] = 0;
+//                     i++;
+//                 }
+//             }
+//         }
+
+//         // Step 2: Pattern Search
+//         int k = 0; // for s2
+//         int l = 0; // for s1
+
+//         while (l < s1.length()) {
+//             if (s1.charAt(l) == s2.charAt(k)) {
+//                 l++;
+//                 k++;
+//                 if (k == s2.length()) return l - k;
+//             } else {
+//                 if (k != 0) {
+//                     k = lps[k - 1];
+//                 } else {
+//                     l++;
+//                 }
+//             }
+//         }
+
+//         return -1;
 //     }
 // }
