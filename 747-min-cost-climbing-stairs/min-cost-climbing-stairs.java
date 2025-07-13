f1f727af80c08d59da1b9679e37aeba63bb1f879
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-13 19:20:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (77.97%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -20,14 +20,29 @@ class Solution {
 
 
     //method-2 bottome-up approach
+    // public int minCostClimbingStairs(int[] cost){
+    //     int n=cost.length;
+    //     int[] dp=new int[n+1];
+    //     dp[0]=0;
+    //     dp[1]=0;
+    //     for(int i=2;i<=n;i++){
+    //         dp[i]=Math.min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+    //     }
+    //     return dp[n];
+    // }
+
+
+    //method-3-> space optimizing
     public int minCostClimbingStairs(int[] cost){
         int n=cost.length;
-        int[] dp=new int[n+1];
-        dp[0]=0;
-        dp[1]=0;
+        int prev=0;
+        int prev2=0;
+        int curr=0;
         for(int i=2;i<=n;i++){
-            dp[i]=Math.min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+            curr=Math.min(cost[i-1]+prev,cost[i-2]+prev2);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n];
+        return curr;
     }
 }
