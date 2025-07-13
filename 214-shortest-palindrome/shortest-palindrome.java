2684eb7ad59026dd305819efb83ae0ad35113343
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-13 10:43:52 +0530

    Time: 10 ms (45.95%) | Memory: 45.9 MB (26.89%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,12 +1,44 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        String rev=new StringBuilder(s).reverse().toString();
+        //method-1
+        // String rev=new StringBuilder(s).reverse().toString();
 
-        for(int i=0;i<s.length();i++){
-            if (s.startsWith(rev.substring(i))){
-                return rev.substring(0,i)+s;
+        // for(int i=0;i<s.length();i++){
+        //     if (s.startsWith(rev.substring(i))){
+        //         return rev.substring(0,i)+s;
+        //     }
+        // }
+        // return rev+s;
+
+
+
+        //method-2
+        //kmp
+
+
+        String rev=new StringBuilder(s).reverse().toString();
+        String temp=s+"-"+rev;
+        int[] lps=new int[temp.length()];
+        lps[0]=0;
+        int len=0;
+        for(int i=1;i<temp.length();){
+            if(temp.charAt(i)==temp.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
             }
         }
-        return rev+s;
+        int largestprefix=lps[temp.length()-1];
+        String t=rev.substring(0,s.length()-largestprefix);
+        return  t+s;
     }
 }
