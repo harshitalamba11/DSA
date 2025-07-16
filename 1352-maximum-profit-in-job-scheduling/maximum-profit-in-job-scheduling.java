4017c32952e2f384349c3d69964189751198e41d
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-16 23:46:03 +0530

    Time: 1266 ms (5.05%) | Memory: 64.9 MB (10.37%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,18 +1,26 @@
 class Solution {
-        public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n = startTime.length;
-        int[][] jobs = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int[][] jobs = new int[startTime.length][3];
+        for (int i = 0; i < startTime.length; i++) {
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
         }
-        Arrays.sort(jobs, (a, b)->a[1] - b[1]);
-        TreeMap<Integer, Integer> dp = new TreeMap<>();
-        dp.put(0, 0);
-        for (int[] job : jobs) {
-            int cur = dp.floorEntry(job[0]).getValue() + job[2];
-            if (cur > dp.lastEntry().getValue())
-                dp.put(job[1], cur);
+        Arrays.sort(jobs,(a,b)->a[0]-b[0]);
+        Map<Integer,Integer> map=new HashMap<>();
+        return recur(map,jobs,0);
+    }
+    public int findnext(int idx,int[][] jobs){
+        for(int next=idx+1;next<jobs.length;next++){
+            if(jobs[next][0]>=jobs[idx][1])return next;
         }
-        return dp.lastEntry().getValue();
+        return -1;
+    }
+    public int recur(Map<Integer,Integer> map,int[][] jobs,int idx){
+        if(idx==jobs.length) return 0;
+        if(map.containsKey(idx)) return map.get(idx); 
+        int next=findnext(idx,jobs);
+        int take=jobs[idx][2]+(next==-1?0:recur(map,jobs,next));
+        int nottake=recur(map,jobs,idx+1);
+        map.put(idx,Math.max(take,nottake));
+        return Math.max(take,nottake);
     }
 }
