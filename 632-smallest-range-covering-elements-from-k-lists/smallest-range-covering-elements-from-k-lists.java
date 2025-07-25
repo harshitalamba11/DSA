aedc4e567a06f81fc9ecbce0830c28db4508477b
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-26 00:45:11 +0530

    Time: 98 ms (57.07%) | Memory: 73 MB (37.26%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -1,53 +1,40 @@
-import java.util.*;
-
-class Tuple {
-    int data, row, col;
-    Tuple(int data, int row, int col) {
-        this.data = data;
-        this.row = row;
-        this.col = col;
+class Tuple{
+    int data,row,col;
+    Tuple(int data,int row,int col){
+        this.data=data;
+        this.row=row;
+        this.col=col;
     }
 }
-
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        PriorityQueue<Tuple> pq = new PriorityQueue<>((a, b) -> a.data - b.data);
-        int max = Integer.MIN_VALUE;
-
-        // Step 1: Push first element of every list into the heap
-        for (int i = 0; i < nums.size(); i++) {
-            int val = nums.get(i).get(0);
-            pq.add(new Tuple(val, i, 0));
-            max = Math.max(max, val);
+        PriorityQueue<Tuple> pq=new PriorityQueue<>((a,b)->a.data-b.data);
+        int min=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.size();i++){
+            pq.add(new Tuple(nums.get(i).get(0),i,0));
+            max=Math.max(max,nums.get(i).get(0));
         }
-
-        int start = pq.peek().data;
-        int end = max;
-        int[] res = new int[]{start, end};
-
-        while (true) {
-            Tuple temp = pq.poll();
-            int min = temp.data;
-
-            // Update range if smaller
-            if (end - start > max - min) {
-                start = min;
-                end = max;
-                res[0] = start;
-                res[1] = end;
+        min=pq.peek().data;
+        int start=min;
+        int end=max;
+        while(true){
+            Tuple temp=pq.poll();
+            int row=temp.row;
+            int col=temp.col;
+            if(end-start<max-min){
+                min=start;
+                max=end;
             }
-
-            // Move to next element in the same list
-            if (temp.col + 1 < nums.get(temp.row).size()) {
-                int nextVal = nums.get(temp.row).get(temp.col + 1);
-                pq.add(new Tuple(nextVal, temp.row, temp.col + 1));
-                max = Math.max(max, nextVal);
-            } else {
-                // One list is exhausted, stop
-                break;
+            if(col+1<nums.get(row).size()){
+                pq.add(new Tuple(nums.get(row).get(col+1),row,col+1));
+                end=Math.max(end,nums.get(row).get(col+1));
+                start=pq.peek().data;
             }
+            else
+            break;
         }
-
+        int[] res=new int[]{min,max};
         return res;
     }
 }
