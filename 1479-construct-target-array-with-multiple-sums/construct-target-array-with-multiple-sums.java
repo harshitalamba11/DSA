4078ae821e4fe3cba6d3cbcad2bce7ef6c405e5f
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-26 11:25:17 +0530

    Time: 8 ms (89.13%) | Memory: 51.2 MB (56.52%) - LeetSync

---
+++ 1479-construct-target-array-with-multiple-sums/construct-target-array-with-multiple-sums.java
@@ -0,0 +1,56 @@
+/*
+class Solution {
+    public boolean isPossible(int[] target) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a); //max-heap
+        //step-2 fill the array elements in the heap and maintain the sum
+        int sum=0;
+        for(int i=0;i<target.length;i++){
+            sum+=target[i];
+            pq.add(target[i]);
+        }
+
+        //this can be the Tle  
+        while(pq.peek()!=1){
+            int maxele=pq.poll();
+            int rem=sum-maxele;
+            if(rem>=maxele || rem<=0){
+                return false;
+            }
+            int ele=maxele-rem;
+            sum-=maxele;
+            pq.add(ele);
+            sum+=ele;
+
+        }
+        return true;
+    }
+}
+*/
+class Solution {
+    public boolean isPossible(int[] target) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a); //max-heap
+        //step-2 fill the array elements in the heap and maintain the sum
+        int sum=0;
+        for(int i=0;i<target.length;i++){
+            sum+=target[i];
+            pq.add(target[i]);
+        }
+
+        //this can be the Tle so to improve line added is mod wali element=maxele%rem 
+        while(pq.peek()!=1){
+            int maxele=pq.poll();
+            int rem=sum-maxele;
+            if(rem>=maxele || rem<=0){
+                return false;
+            }
+            // int ele=maxele-rem;
+            int ele=maxele%rem;
+            if(ele<=0 && rem!=1) return false;
+            sum-=maxele;
+            pq.add(ele);
+            sum+=ele;
+
+        }
+        return true;
+    }
+}
