79badcb82638716f5592c906ab2131bee6df6a0e
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-26 22:37:25 +0530

    Time: 15 ms (66.35%) | Memory: 46.8 MB (5.20%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,23 +1,43 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         Map<Character,Integer> map=new HashMap<>();
-        for (char task : tasks) {
-            map.put(task, map.getOrDefault(task, 0) + 1);
-        }
-        int res=0;
         int max=0;
-        int l=map.size();
-        for (int freq : map.values()) {
-            max = Math.max(max, freq);
+        for(int i=0;i<tasks.length;i++){
+            map.put(tasks[i],map.getOrDefault(tasks[i],0)+1);
         }
-        int count=0;
-        for (int freq : map.values()) {
-            if (freq == max) {
-                count++;
+        for(char i:map.keySet()){
+            max=Math.max(max,map.get(i));
+        }
+        int sum=(max-1)*(n+1);
+        for(char i:map.keySet()){
+            if(map.get(i)==max){
+                sum+=1;
             }
         }
-        res=(max-1)*(n+1);
-        res+=count;
-        return Math.max(res,tasks.length);
+        return Math.max(sum, tasks.length);
+
+
+
+
+
+        // Map<Character,Integer> map=new HashMap<>();
+        // for (char task : tasks) {
+        //     map.put(task, map.getOrDefault(task, 0) + 1);
+        // }
+        // int res=0;
+        // int max=0;
+        // int l=map.size();
+        // for (int freq : map.values()) {
+        //     max = Math.max(max, freq);
+        // }
+        // int count=0;
+        // for (int freq : map.values()) {
+        //     if (freq == max) {
+        //         count++;
+        //     }
+        // }
+        // res=(max-1)*(n+1);
+        // res+=count;
+        // return Math.max(res,tasks.length);
     }
 }
