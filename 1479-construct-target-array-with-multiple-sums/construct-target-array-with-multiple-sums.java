cd8454af47346bab3d5167db15cdea13efc34004
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-26 11:30:54 +0530

    Time: 8 ms (89.13%) | Memory: 51 MB (63.04%) - LeetSync

--- 1479-construct-target-array-with-multiple-sums/construct-target-array-with-multiple-sums.java
+++ 1479-construct-target-array-with-multiple-sums/construct-target-array-with-multiple-sums.java
@@ -36,7 +36,8 @@ class Solution {
             pq.add(target[i]);
         }
 
-        //this can be the Tle so to improve line added is mod wali element=maxele%rem 
+        //this can be the Tle so to improve line added is mod wali element=maxele%rem + test case need to understand
+        // the rem case logic is that example [1,1,0] aaya toh hmara [1,1,1] to  kabhi bna gga hi nhi but [1,0] hua toh sum 1 hai means we can say ki iske previous step meh yeh [1,1] tha then [1,0] hua means [1,1] toh bna hi na....:)
         while(pq.peek()!=1){
             int maxele=pq.poll();
             int rem=sum-maxele;
