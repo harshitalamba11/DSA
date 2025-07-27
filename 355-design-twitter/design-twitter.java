d829641f02dfc0b3be9210c4941afeb47bfb815c
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-07-27 18:15:57 +0530

    Time: 32 ms (68.61%) | Memory: 54.8 MB (62.85%) - LeetSync

--- 355-design-twitter/design-twitter.java
+++ 355-design-twitter/design-twitter.java
@@ -1,53 +1,64 @@
+
+class Tweet{
+    int tweetid;
+    int time=0;
+    Tweet(int tweetid,int time){
+        this.tweetid=tweetid;
+        this.time=time;
+    }
+}
 class Twitter {
-    private static int time = 0;
-
-    private class Tweet {
-        int id;
-        int timestamp;
-        Tweet(int id, int time) {
-            this.id = id;
-            this.timestamp = time;
-        }
-    }
-
-    Map<Integer, List<Tweet>> tweets = new HashMap<>();
-    Map<Integer, Set<Integer>> follows = new HashMap<>();
-
-    public Twitter() { }
-
+    public int time=1;
+    Map<Integer,List<Tweet>> map1=new HashMap<>();
+    Map<Integer,HashSet<Integer>> map2=new HashMap<>();
+    public Twitter() {}
     public void postTweet(int userId, int tweetId) {
-        tweets.putIfAbsent(userId, new ArrayList<>());
-        tweets.get(userId).add(new Tweet(tweetId, time++));
+        map1.putIfAbsent(userId,new ArrayList<>());
+        map1.get(userId).add(new Tweet(tweetId,time++));
     }
-
+    
     public List<Integer> getNewsFeed(int userId) {
-        PriorityQueue<Tweet> pq = new PriorityQueue<>((a, b) -> a.timestamp - b.timestamp);
-        follows.putIfAbsent(userId, new HashSet<>());
-        follows.get(userId).add(userId); // include own tweets
+        List<Integer> res=new ArrayList<>();
 
-        for (int user : follows.get(userId)) {
-            List<Tweet> userTweets = tweets.getOrDefault(user, new ArrayList<>());
-            for (Tweet tweet : userTweets) {
-                pq.offer(tweet);
-                if (pq.size() > 10) pq.poll();
-            }
+        PriorityQueue<Tweet> pq=new PriorityQueue<>((a,b)->b.time-a.time);
+
+
+        pq.addAll(map1.getOrDefault(userId,new ArrayList<>()));
+
+        HashSet<Integer> followees = map2.getOrDefault(userId, new HashSet<>());
+        for (int followeeId : followees) {
+            List<Tweet> tweets = map1.getOrDefault(followeeId, new ArrayList<>());
+            pq.addAll(tweets);
         }
 
-        List<Integer> res = new ArrayList<>();
-        while (!pq.isEmpty()) res.add(pq.poll().id);
-        Collections.reverse(res);
+
+        int k=10;
+        while(k>0 && !pq.isEmpty()){
+            Tweet t=pq.poll();
+            res.add(t.tweetid);
+            k--;
+        }
+        // Collections.reverse(res);
         return res;
     }
-
+    
     public void follow(int followerId, int followeeId) {
-        follows.putIfAbsent(followerId, new HashSet<>());
-        follows.get(followerId).add(followeeId);
+        map2.putIfAbsent(followerId,new HashSet<>());
+        map2.get(followerId).add(followeeId);
     }
-
+    
     public void unfollow(int followerId, int followeeId) {
-        if (followerId == followeeId) return; // can't unfollow yourself
-        if (follows.containsKey(followerId)) {
-            follows.get(followerId).remove(followeeId);
+        if(map2.containsKey(followerId)){
+            map2.get(followerId).remove(followeeId);
         }
     }
 }
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
