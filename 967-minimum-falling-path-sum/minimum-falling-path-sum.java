dfc21338be602893f60d1cde5732a6a407e55849
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-08-02 22:18:31 +0530

    Time: 7 ms (57.97%) | Memory: 45.7 MB (28.46%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,26 +1,22 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
-        int[][] dp = new int[n][n];
-
-        // Copy last row
-        for (int i = 0; i < n; i++) {
-            dp[n-1][i] = matrix[n-1][i];
+        int[][] dp=new int[matrix.length][matrix.length];
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=matrix[n-1][i];
         }
-
-        for (int row = n - 2; row >= 0; row--) {
-            for (int col = 0; col < n; col++) {
-                int down = dp[row + 1][col];
-                int leftDiagonal = col > 0 ? dp[row + 1][col - 1] : Integer.MAX_VALUE;
-                int rightDiagonal = col < n - 1 ? dp[row + 1][col + 1] : Integer.MAX_VALUE;
-
-                dp[row][col] = matrix[row][col] + Math.min(down, Math.min(leftDiagonal, rightDiagonal));
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<n;j++){
+                int down=dp[i+1][j];
+                int left_diagonal=j-1>=0?dp[i+1][j-1]:Integer.MAX_VALUE;
+                int right_diagonal=j+1<n?dp[i+1][j+1]:Integer.MAX_VALUE;
+            
+            dp[i][j] = matrix[i][j] + Math.min(down, Math.min(left_diagonal, right_diagonal));
             }
         }
-
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            min = Math.min(min, dp[0][i]);
+        int min=Integer.MAX_VALUE;
+        for(int k=0;k<n;k++){
+            min=Math.min(min,dp[0][k]);
         }
         return min;
     }
