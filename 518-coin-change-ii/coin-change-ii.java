8ac73ee40950866a88d3ad2325101828bdb5a1df
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-08-03 21:54:37 +0530

    Time: 23 ms (11.31%) | Memory: 51.5 MB (8.15%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,13 +1,12 @@
 class Solution {
-    public int recur(int[] coins,int amount,int idx,Integer[][] dp){
-        if(amount==0) return 1;
-        if(amount<0) return 0;
-        if(idx>=coins.length) return 0;
-        if (dp[idx][amount] != null) return dp[idx][amount];
-        return dp[idx][amount]=recur(coins,amount-coins[idx],idx,dp)+recur(coins,amount,idx+1,dp);
+    public int recur(int amount,int[] coins,int sum,int idx,Integer[][] dp){
+        if(sum==amount) return 1;
+        if (sum > amount || idx >= coins.length) return 0;
+        if(dp[idx][sum]!=null) return dp[idx][sum];
+        return dp[idx][sum]=recur(amount,coins,sum,idx+1,dp)+ recur(amount,coins,sum+coins[idx],idx,dp);
     }
     public int change(int amount, int[] coins) {
         Integer[][] dp = new Integer[coins.length][amount + 1];
-        return recur(coins,amount,0,dp);
+        return recur(amount,coins,0,0,dp);
     }
 }
