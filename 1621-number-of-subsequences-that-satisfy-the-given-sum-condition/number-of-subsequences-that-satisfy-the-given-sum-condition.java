ee931d89073f6b488c439c50f11aceee48a13c19
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-08-03 16:23:17 +0530

    Time: 27 ms (98.44%) | Memory: 55.3 MB (50.60%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,18 +1,18 @@
 class Solution {
     public int numSubseq(int[] nums, int target) {
-        int l=0;
-        Arrays.sort(nums);
         int mod = 1_000_000_007;
+        int l=0;
+        int n=nums.length;
         int r=nums.length-1;
         int c=0;
-        int[] pow=new int[nums.length];
+        Arrays.sort(nums);
+        int[] pow=new int[n];
         pow[0]=1;
-        for(int i=1;i<nums.length;i++){
-            pow[i]=pow[i-1]*2%mod;
+        for(int i=1;i<n;i++){
+            pow[i] = (pow[i - 1] * 2) % mod;
         }
         while(l<=r){
             if(nums[l]+nums[r]<=target){
-                
                 c=(c+pow[r-l])%mod;
                 l++;
             }
