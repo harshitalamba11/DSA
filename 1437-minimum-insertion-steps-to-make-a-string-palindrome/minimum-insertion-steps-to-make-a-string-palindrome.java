996b288ca54590b815cade0548c93e333cfc3bd1
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-08-06 12:17:11 +0530

    Time: 21 ms (77.04%) | Memory: 47.1 MB (5.30%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,22 +1,37 @@
 class Solution {
-    public int minInsertions(String s) {
-        int[] prev=new int[s.length()+1];
-        int[] curr=new int[s.length()+1];
-        StringBuilder str=new StringBuilder(s);
-        str.reverse();
-        for(int i=1;i<=s.length();i++){
-            for(int j=1;j<=s.length();j++){
-                if(s.charAt(i-1)==str.charAt(j-1)){
-                    curr[j]=1+prev[j-1];
-                }
-                else{
-                    curr[j]=Math.max(prev[j],curr[j-1]);
-                }
-            }
-            int temp[]=prev;
-            prev=curr;
-            curr=temp;
+    public int recur(String s,Integer[][] dp,int i,int j){
+        if(i>=j) return 0;
+        if(dp[i][j]!=null) return dp[i][j];
+        if(s.charAt(i)==s.charAt(j)){
+            dp[i][j]=recur(s,dp,i+1,j-1);
         }
-        return s.length()-prev[s.length()];
+        else{
+            dp[i][j]=1+Math.min(recur(s,dp,i+1,j),recur(s,dp,i,j-1));
+        }
+        return dp[i][j];
+    }
+    public int minInsertions(String s) {
+        //approach -2 not better than  down waali
+        Integer[][] dp=new Integer[s.length()+1][s.length()+1];
+        return recur(s,dp,0,s.length()-1);
+
+        // int[] prev=new int[s.length()+1];
+        // int[] curr=new int[s.length()+1];
+        // StringBuilder str=new StringBuilder(s);
+        // str.reverse();
+        // for(int i=1;i<=s.length();i++){
+        //     for(int j=1;j<=s.length();j++){
+        //         if(s.charAt(i-1)==str.charAt(j-1)){
+        //             curr[j]=1+prev[j-1];
+        //         }
+        //         else{
+        //             curr[j]=Math.max(prev[j],curr[j-1]);
+        //         }
+        //     }
+        //     int temp[]=prev;
+        //     prev=curr;
+        //     curr=temp;
+        // }
+        // return s.length()-prev[s.length()];
     }
 }
