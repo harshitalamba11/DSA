7fafae41038516d0dc7b32647389418894214391
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-08-07 14:37:41 +0530

    Time: 4 ms (5.79%) | Memory: 50.6 MB (5.16%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,34 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int sum=0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]>=prices[i-1]){
-                sum+=(prices[i]-prices[i-1]);
-            }
+
+        //successful method-1
+        // int sum=0;
+        // for(int i=1;i<prices.length;i++){
+        //     if(prices[i]>=prices[i-1]){
+        //         sum+=(prices[i]-prices[i-1]);
+        //     }
+        // }
+        // return sum;
+
+
+        //method-2
+        Integer[][] dp=new Integer[prices.length+1][2];
+        return recur(prices,0,1,dp);   //1 for buy 0 for sell
+    }
+    public int recur(int[] prices,int idx,int buy,Integer[][] dp){
+        if(idx==prices.length) return 0;
+        if(dp[idx][buy]!=null) return dp[idx][buy];
+        int profit=0;
+        if(buy==1){
+            int will_buy=-prices[idx]+recur(prices,idx+1,0,dp);
+            int will_not_buy=0+recur(prices,idx+1,1,dp);
+            profit=Math.max(will_buy,will_not_buy);
         }
-        return sum;
+        else{
+            int will_sell=prices[idx]+recur(prices,idx+1,1,dp);
+            int will_not_sell=0+recur(prices,idx+1,0,dp);
+            profit=Math.max(will_sell,will_not_sell);
+        }
+        return dp[idx][buy]=profit;
     }
 }
