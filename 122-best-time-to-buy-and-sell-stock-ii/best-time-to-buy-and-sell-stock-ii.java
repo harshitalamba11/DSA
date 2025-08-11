a0b46db649b6581570b0adfe95bc79a0eee67a69
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-08-12 02:03:24 +0530

    Time: 1 ms (77.51%) | Memory: 46.3 MB (15.00%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,5 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit=0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]>prices[i-1])
-            profit+=prices[i]-prices[i-1];
-        }
-        return profit;
         //successful method-1
         // int sum=0;
         // for(int i=1;i<prices.length;i++){
@@ -22,23 +16,37 @@ class Solution {
 
 
         //method-3
-        // int[][] dp=new int[prices.length+1][2];
-        // return recur(prices,dp);
+        int[][] dp=new int[prices.length+1][2];
+        return recur(prices,dp);
     }
     public int recur(int[] prices,int[][] dp){
         for(int i=prices.length-1;i>=0;i--){
             for(int j=0;j<=1;j++){
                 if(j==0){
                     int will_buy=-prices[i]+dp[i+1][1];
-                    int will_not_buy=0+dp[i+1][0];
-                    dp[i][j]=Math.max(will_buy,will_not_buy);
+                    int will_notbuy=0+dp[i+1][0];
+                    dp[i][j]=Math.max(will_buy,will_notbuy);
                 }
                 else{
                     int will_sell=prices[i]+dp[i+1][0];
-                    int will_not_sell=0+dp[i+1][1];
-                    dp[i][j]=Math.max(will_sell,will_not_sell);
+                    int will_notsell=0+dp[i+1][1];
+                    dp[i][j]=Math.max(will_sell,will_notsell);
                 }
-            }        
+                
+        }
+        // for(int i=prices.length-1;i>=0;i--){
+        //     for(int j=0;j<=1;j++){
+        //         if(j==0){
+        //             int will_buy=-prices[i]+dp[i+1][1];
+        //             int will_not_buy=0+dp[i+1][0];
+        //             dp[i][j]=Math.max(will_buy,will_not_buy);
+        //         }
+        //         else{
+        //             int will_sell=prices[i]+dp[i+1][0];
+        //             int will_not_sell=0+dp[i+1][1];
+        //             dp[i][j]=Math.max(will_sell,will_not_sell);
+        //         }
+        //     }        
         }
         
         return dp[0][0];
