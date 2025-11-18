d8927a19ea531cfe2e0d82ff331bd78deccfc56a
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-11-18 13:55:23 +0530

    Time: 21 ms (75.75%) | Memory: 109.4 MB (24.25%) - LeetSync

---
+++ 2176-parallel-courses-iii/parallel-courses-iii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minimumTime(int n, int[][] relations, int[] time) {
+        int months=0;
+        Queue<Integer> q=new LinkedList<>();
+        int indeg[]=new int[n];
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int[] d:relations){
+            adj.get(d[0]-1).add(d[1]-1);
+            indeg[d[1]-1]++;
+        }
+        int fin[]=new int[n];
+        for(int i=0;i<indeg.length;i++){
+            if(indeg[i]==0){
+                fin[i]=time[i];
+                q.add(i);
+            }
+        }
+        boolean[] vis=new boolean[n];
+        
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            months=Math.max(fin[curr],months);
+            int max=0;
+            for(int des:adj.get(curr)){
+                indeg[des]--;
+                fin[des]=Math.max(fin[des],fin[curr]+time[des]);
+                if(indeg[des]==0){
+                    
+                    q.add(des);
+                }
+            }
+
+            // months+=max;
+        }
+        return months;
+    }
+}
