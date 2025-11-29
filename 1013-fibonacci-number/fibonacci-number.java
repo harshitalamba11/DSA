c7c4c2f967ae04131b2e0850dd7739659515452d
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-11-29 18:10:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (10.66%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,15 +1,12 @@
 class Solution {
     public int fib(int n) {
+        if(n<=0) return 0;
         int[] dp=new int[n+1];
-        //using recur simple because understanding dp-> top-down
-        return recur(n,dp);
-    }
-    public int recur(int n,int[] dp){
-        if(n==0) return dp[0]=0;
-        if(n<=1){
-            return dp[n]=1;
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(dp[n]!=0) return dp[n];
-        return dp[n]=recur(n-1,dp)+recur(n-2,dp);
+        return dp[n];
     }
 }
