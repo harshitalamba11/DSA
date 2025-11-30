ad79d65e36c7660d0b7a427ea391cdeb92cfe7c8
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-12-01 00:27:21 +0530

    Time: 3 ms (88.70%) | Memory: 83 MB (35.98%) - LeetSync

--- 2591-frog-jump-ii/frog-jump-ii.java
+++ 2591-frog-jump-ii/frog-jump-ii.java
@@ -1,30 +1,12 @@
 class Solution {
     public int maxJump(int[] stones) {
-        // int n = stones.length;
-        // int maxJump = 0;
-
-        // // Go through the stones with step size 2 (zigzag pattern)
-        // for (int i = 2; i < n; i++) {
-        //     // Calculate the jump between every alternate stone
-        //     maxJump = Math.max(maxJump, stones[i] - stones[i - 2]);
-        // }
-
-        // // Also consider the first jump (stone[1] - stone[0])
-        // maxJump = Math.max(maxJump, stones[1] - stones[0]);
-
-        // // Also consider the last jump (stone[n-1] - stone[n-2])
-        // maxJump = Math.max(maxJump, stones[n - 1] - stones[n - 2]);
-
-        // return maxJump;
-
-
-        int n=stones.length;
-        int maxjump=0;
+        int max=0;int n=stones.length;
         for(int i=2;i<n;i++){
-            maxjump=Math.max(maxjump,stones[i]-stones[i-2]);
+            max=Math.max(max,stones[i]-stones[i-2]);
         }
-        maxjump=Math.max(maxjump,stones[1]-stones[0]);
-        maxjump=Math.max(maxjump,stones[n-1]-stones[n-2]);
-        return maxjump;
+        for(int i=1;i<n;i++){
+            max=Math.max(max,stones[i]-stones[i-1]);
+        }
+        return max;
     }
 }
