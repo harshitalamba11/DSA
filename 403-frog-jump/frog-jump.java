1e36686f83bfcb6bc64eebc7b8e068efe9960f9d
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-11-30 23:47:07 +0530

    Time: 32 ms (73.73%) | Memory: 146.6 MB (8.17%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -1,25 +1,55 @@
 class Solution {
+    public boolean result = false;
+
     public boolean canCross(int[] stones) {
-        Map<Integer,HashSet<Integer>> map=new HashMap<>();
-        for(int i=0;i<stones.length;i++){
-            map.put(stones[i],new HashSet<>());
+
+        int n = stones.length;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(stones[i], i);
         }
-        map.get(stones[0]).add(1);
-        for(int i=0;i<stones.length;i++){
-            int curr=stones[i];
-            Set<Integer> jumps=new HashSet(map.get(curr));
-            for(int jump:jumps){
-                int pos=jump+curr;
-                if(pos==stones[stones.length-1]){
+
+        int[][] dp = new int[n][n + 1];
+        for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+
+        return recur(stones, map, 0, 0, dp);
+    }
+
+    public boolean recur(int[] stones, Map<Integer, Integer> map,
+                         int curr_idx, int prev_jump, int[][] dp) {
+
+        int n = stones.length;
+
+        // memo check
+        if (dp[curr_idx][prev_jump] != -1)
+            return dp[curr_idx][prev_jump] == 1;
+
+        // reached final stone
+        if (curr_idx == n - 1) {
+            dp[curr_idx][prev_jump] = 1;
+            return true;
+        }
+
+        // try jumps
+        for (int jump = prev_jump - 1; jump <= prev_jump + 1; jump++) {
+
+            if (jump <= 0) continue;
+
+            int next_pos = stones[curr_idx] + jump;
+
+            if (map.containsKey(next_pos)) {
+
+                int next_idx = map.get(next_pos);
+
+                if (recur(stones, map, next_idx, jump, dp)) {
+                    dp[curr_idx][prev_jump] = 1;     // FIXED: store at curr_idx, prev_jump
                     return true;
-                }
-                if(map.containsKey(pos)){
-                    if(jump-1>0) map.get(pos).add(jump-1);
-                    map.get(pos).add(jump);
-                    map.get(pos).add(jump+1);
                 }
             }
         }
+
+        dp[curr_idx][prev_jump] = 0;                 // mark as impossible
         return false;
     }
 }
