aa5f913c2c04f73384f0a1019cd8a79ac605f2d0
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-12-01 19:23:00 +0530

    Time: 1 ms (99.74%) | Memory: 50.1 MB (37.18%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,20 +1,25 @@
 class Solution {
-    public int recur(int[][] grid,int m,int n,int min,int row,int col,int sum,int[][] dp){
-        if(row>=m || col>=n) return min;
-        if(row==m-1 && col==n-1){
-            return grid[row][col];
-        }
-        if (dp[row][col] != -1) return dp[row][col];
-        int down=recur(grid,m,n,min,row+1,col,sum,dp);
-        int right=recur(grid,m,n,min,row,col+1,sum,dp);
-        return dp[row][col] = grid[row][col] + Math.min(down, right);
-    }
+    public int max_sum=0;
     public int minPathSum(int[][] grid) {
-        int min=Integer.MAX_VALUE;
-        int m=grid.length;
-        int n=grid[0].length;
-        int[][] dp=new int[m+1][n+1];
-        for(int[] l:dp) Arrays.fill(l,-1);
-        return recur(grid,m,n,min,0,0,0,dp);
+        int n=grid.length;
+        int m=grid[0].length;
+
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return recur(grid,n,m,0,0,dp);
+    }
+    public int recur(int[][] grid,int n,int m,int i,int j,int[][] dp){
+        if(i >= n || j >= m) return Integer.MAX_VALUE;
+        if(i==n-1 && j==m-1){
+            return grid[i][j];
+        }
+        if(dp[i][j] != -1) 
+            return dp[i][j];
+
+        int down=recur(grid,n,m,i,j+1,dp);
+        int right=recur(grid,n,m,i+1,j,dp);
+        return dp[i][j]=grid[i][j]+Math.min(down,right);
     }
 }
