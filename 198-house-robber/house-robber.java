4a8b6de98cfec65cbe45c674bb1ef639aa1bbf6e
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-12-01 14:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (12.85%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,61 +1,21 @@
 class Solution {
-    // public int recur(int[] nums,int idx,int[] dp){
-    //     //method-1 ->>> top-down approach
-    //     if(idx==0) return nums[0];
-    //     if(idx<0){
-    //         return 0;
-    //     }
-    //     if(dp[idx]!=-1) return dp[idx];
-    //     return dp[idx]=Math.max((nums[idx]+recur(nums,idx-2,dp)),recur(nums,idx-1,dp));
-    // }
-    // public int rob(int[] nums) {
-    //     int[] dp=new int[nums.length+1];
-    //     Arrays.fill(dp,-1);
-    //     return recur(nums,nums.length-1,dp);
-    // }
-
-
-
-    //method-2 ->>> bottome-up approach
-    // public int rob(int[] nums) {
-    //     int[] dp=new int[nums.length+1];
-    //     if(nums.length==1) return nums[0];
-    //     if(nums.length<=2) return Math.max(nums[0],nums[1]);
-    //     dp[0]=nums[0];dp[1]=Math.max(nums[0],nums[1]);
-    //     for(int i=2;i<nums.length;i++){
-    //         dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
-    //     }
-    //     return dp[nums.length-1];
-    // }
-
-
-    //method-3 ->>> space-complexity
-    // public int rob(int[] nums){
-    //     int prev=nums[0];
-    //     if(nums.length==1) return nums[0];
-    //     if(nums.length==2) return Math.max(nums[0],nums[1]);
-    //     int next=Math.max(nums[0],nums[1]);
-    //     int curr=0;
     
-    //     for(int i=2;i<nums.length;i++){
-    //         curr=Math.max(nums[i]+prev,next);
-    //         prev=next;
-    //         next=curr;
-    //     }
-    //     return curr;
-    // }
-
-    public int recur(int[] nums,int idx,int n,int[] dp){
-        if(idx>=n){
-            return 0;
+    public int recur(int[] nums,int n,int[] dp){
+        // if(dp[idx]!=-1) return dp[idx];
+        for(int i=2;i<=n;i++){
+            int not_steal=dp[i-1];
+            int steal=0;
+            if(i>1)
+            steal=dp[i-2]+nums[i-1];
+            dp[i]=Math.max(steal,not_steal);
         }
-        if(dp[idx]!=-1) return dp[idx];
-        return dp[idx]=Math.max(nums[idx]+recur(nums,idx+2,n,dp),recur(nums,idx+1,n,dp));
+        return dp[n];
     }
     public int rob(int[] nums) {
-        int[] dp=new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        return recur(nums,0,nums.length,dp);
+        int n=nums.length;
+        int[] dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        return recur(nums,n,dp);
     }
-    
 }
