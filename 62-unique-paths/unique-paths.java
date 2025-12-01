150b38e7ff18a49300ae9f4b02d04708e23b9113
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-12-01 18:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (29.62%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int recur(int m,int n,int row,int col,int[][] dp){
-        if(row >= m || col >= n) return 0;
-        if(row==m-1 && col==n-1) return 1;
-        if(dp[row][col]!=-1) return dp[row][col];
-        int right=recur(m,n,row,col+1,dp);
-        int down=recur(m,n,row+1,col,dp);
-        return dp[row][col]=right+down;
+    public int recur(int m,int n,int i,int j,int[][] dp){
+        if(i>=m || j>=n) return 0;
+        if(i==m-1 && j==n-1) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down=recur(m,n,i,j+1,dp);
+        int right=recur(m,n,i+1,j,dp);
+        return dp[i][j]=down+right;
     }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m+1][n+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
+        for(int i=0;i<m;i++) Arrays.fill(dp[i],-1);
         return recur(m,n,0,0,dp);
     }
 }
