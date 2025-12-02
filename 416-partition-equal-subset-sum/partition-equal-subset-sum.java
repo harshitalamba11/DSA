b244a5410df594163d73e3448f056bd096f462c2
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-12-02 20:56:57 +0530

    Time: 60 ms (39.33%) | Memory: 126.5 MB (5.00%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,17 +1,18 @@
 class Solution {
-    public boolean recur(int[] nums,int target,int sum,int idx,Boolean[][] dp){
-        if(idx>=nums.length) return false;
-        if(dp[idx][sum]!=null) return dp[idx][sum];
-        if(sum==target) return true;
-        return dp[idx][sum]=recur(nums,target,sum+nums[idx],idx+1,dp)||recur(nums,target,sum,idx+1,dp);
-    }
     public boolean canPartition(int[] nums) {
+        int n=nums.length;
         int target=0;
-        for(int i=0;i<nums.length;i++){
-            target+=nums[i];
-        }
-        if(target%2!=0) return false;
+        for(int i=0;i<n;i++) target+=nums[i];
         Boolean[][] dp=new Boolean[nums.length][target+1];
-        return recur(nums,target/2,0,0,dp);
+        if(target%2!=0) return false;
+        return recur(nums,target/2,0,dp,0);
+    }
+    public boolean recur(int[] nums,int target,int sum,Boolean[][] dp,int idx){
+        if(idx>=nums.length){
+            return false;
+        }
+        if(sum==target) return true;
+        if(dp[idx][sum]!=null) return dp[idx][sum];
+        return dp[idx][sum]=recur(nums,target,sum+nums[idx],dp,idx+1)||recur(nums,target,sum,dp,idx+1);
     }
 }
