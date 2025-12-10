a89a3fc88c06d513d05a72b542f355a8f7f84900
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-12-11 00:23:12 +0530

    Time: 20 ms (78.74%) | Memory: 47.9 MB (5.67%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,37 +1,17 @@
 class Solution {
-    public int recur(String s,Integer[][] dp,int i,int j){
-        if(i>=j) return 0;
-        if(dp[i][j]!=null) return dp[i][j];
-        if(s.charAt(i)==s.charAt(j)){
-            dp[i][j]=recur(s,dp,i+1,j-1);
-        }
-        else{
-            dp[i][j]=1+Math.min(recur(s,dp,i+1,j),recur(s,dp,i,j-1));
-        }
-        return dp[i][j];
-    }
     public int minInsertions(String s) {
-        //approach -2 not better than  down waali
-        Integer[][] dp=new Integer[s.length()+1][s.length()+1];
-        return recur(s,dp,0,s.length()-1);
-
-        // int[] prev=new int[s.length()+1];
-        // int[] curr=new int[s.length()+1];
-        // StringBuilder str=new StringBuilder(s);
-        // str.reverse();
-        // for(int i=1;i<=s.length();i++){
-        //     for(int j=1;j<=s.length();j++){
-        //         if(s.charAt(i-1)==str.charAt(j-1)){
-        //             curr[j]=1+prev[j-1];
-        //         }
-        //         else{
-        //             curr[j]=Math.max(prev[j],curr[j-1]);
-        //         }
-        //     }
-        //     int temp[]=prev;
-        //     prev=curr;
-        //     curr=temp;
-        // }
-        // return s.length()-prev[s.length()];
+        int l=s.length();
+        int[][]  dp=new int[l+1][l+1];
+        for(int i=0;i<l+1;i++) Arrays.fill(dp[i],-1);
+        return recur(s,0,l-1,dp);
+    }
+    public int recur(String s,int i,int j,int[][] dp){
+        if(i==j) return 0;
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s.charAt(i)==s.charAt(j)){
+            return dp[i][j]=recur(s,i+1,j-1,dp);
+        }
+        return dp[i][j]=1 + Math.min(recur(s, i+1, j,dp), recur(s, i, j-1,dp));
     }
 }
