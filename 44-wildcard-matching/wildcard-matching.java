dff47a7b4e0866b9287edd9a7ab42080d0f68c85
harshitalamba11 <157495617+harshitalamba11@users.noreply.github.com>
2025-12-11 13:01:52 +0530

    Time: 86 ms (7.78%) | Memory: 63.9 MB (5.11%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -4,20 +4,28 @@ class Solution {
         return recur(s,p,0,0,dp);
     }
     public boolean recur(String s,String p,int i,int j,Boolean[][] dp){
-        if(i==s.length() && j==p.length()) return true;
-        if(j==p.length()) return false;
+        if(i==s.length() && j==p.length()) return dp[i][j]=true;
+        if(j==p.length()) return dp[i][j]=false;
+
         if(dp[i][j]!=null) return dp[i][j];
+
+        //if string finish
         if(i==s.length()){
-            while(j<p.length() && p.charAt(j)=='*'){
+            while(j!=p.length() && p.charAt(j)=='*'){
                 j++;
             }
-            return j==p.length();
+            return dp[i][j]=(j==p.length()?true:false);
         }
+
+        //if string finish nhi hua
+        //pattern keh paas ? hai
         if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
             return dp[i][j]=recur(s,p,i+1,j+1,dp);
-        } 
-        else if(p.charAt(j)=='*'){
-            return dp[i][j]=(j<p.length() && recur(s,p,i,j+1,dp) || (i<s.length() && recur(s,p,i+1,j,dp)));
+        }
+        
+        //pattern keh paas * hai
+        if(p.charAt(j)=='*'){
+            return dp[i][j]=(i<s.length() && recur(s,p,i+1,j,dp))||(j<p.length() && recur(s,p,i,j+1,dp));
         }
         return dp[i][j]=false;
     }
